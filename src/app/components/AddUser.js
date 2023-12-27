44512705038387964bb9942394ecbc765c8bdb67
Aditya Dhanraj <opsingh861@gmail.com>
2023-12-27 17:32:51 +0530

    integrated redux store with fronend to show the data

--- src/app/components/AddUser.js
+++ src/app/components/AddUser.js
@@ -1,10 +1,22 @@
+"use client"
+
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../redux/slice";
+
 
 export default function AddUser() {
+    const [name,setName] = useState('');
+    const dispatch = useDispatch();
+    const userDispatcher = () => {
+        dispatch(addUser(name))
+        console.log(name);
+    }
     return (
         <div className="add-user">
             <h1>Add User</h1>
-            <input className="input-box" type="text" placeholder="Enter Name" />
-            <button className="btn">Add</button>
+            <input onChange={(e)=>setName(e.target.value)} className="input-box" type="text" placeholder="Enter Name" />
+            <button onClick={userDispatcher} className="btn">Add</button>
         </div>
     )
 }
--- src/app/components/DisplayUser.js
+++ src/app/components/DisplayUser.js
@@ -1,10 +1,19 @@
+"use client"
 
+import { useSelector } from "react-redux";
 
 export default function DisplayUser() {
+    const users = useSelector(state => state.users);
     return (
         <div className="display-user">
             <h1>Display Users</h1>
-
+            {
+                users.map((user) => (
+                    <div key={user.id} className="user">
+                        <h3>{user.name}</h3>
+                    </div>
+                ))
+            }
         </div>
     )
 }
