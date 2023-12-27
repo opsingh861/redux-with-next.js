9e34848204666bf38516f46aa053695f0a31f1ca
Aditya Dhanraj <opsingh861@gmail.com>
2023-12-27 20:39:53 +0530

    storing data in local storage and removing from it

--- src/app/redux/slice.js
+++ src/app/redux/slice.js
@@ -1,7 +1,7 @@
-const { createSlice, nanoid } = require("@reduxjs/toolkit");
+const { createSlice, nanoid, current } = require("@reduxjs/toolkit");
 
 const initialState = {
-    users: []
+    users: JSON.parse(localStorage.getItem('users')) || []
 };
 
 const Slice = createSlice({
@@ -15,14 +15,22 @@ const Slice = createSlice({
                 name: action.payload,
             }
             state.users.push(data);
+            const userData = JSON.stringify(current(state.users));
+            localStorage.setItem('users', userData);
         },
 
         removeUser: (state, action) => {
             const data = state.users.filter((user) => user.id !== action.payload);
             state.users = data;
+            var storedData = localStorage.getItem('users');
+            var userData = JSON.parse(storedData);
+            userData = userData.filter((user) => user.id !== action.payload);
+            const newData = JSON.stringify(userData);
+            localStorage.setItem('users', newData);
+
         }
     }
 });
 
-export const { addUser,removeUser } = Slice.actions;
+export const { addUser, removeUser } = Slice.actions;
 export default Slice.reducer;
